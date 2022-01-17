e7f3e24646c53e537c7b14ab09c1066615554380
Invelox Tecnologias <contato@invelox.com.br>
2022-01-17 01:46:47 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    byte byteP = 1;
+    short shortP = 1;
+    int intP = 1;
+    long longP = 10L;
+    float floatP = 10F;
+    double doubleP = 10D;
+    char charP = 'W';
+    boolean booleanP = false;
+
+    Byte byteW = 1;
+    Short shortW = 1;
+    Integer intW = 1;
+    Long longW = 10L;
+    Float floatW = 10F;
+    Double doubleW = 10D;
+    Character charW = 'W';
+    Boolean booleanW = false;
+}
