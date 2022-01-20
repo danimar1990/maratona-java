0a8c932dd3d30aee4e83fa909bd43900bf244edb
Invelox Tecnologias <contato@invelox.com.br>
2022-01-19 22:24:01 -0300

    117 - Classes Utilitárias - Internacionalização de moeda com Locale

--- src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -1,29 +1,25 @@
 package academy.devdojo.maratonajava.javacore.Sformatacao.test;
 
-import academy.devdojo.maratonajava.javacore.Gassociacao.model.Local;
-
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeDefault = Locale.getDefault();
         Locale localeBR = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localeBR);
-        nfa[3] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
         double valor = 10_000.2130;
         for(NumberFormat numberFormat : nfa) {
-            numberFormat.setMaximumFractionDigits(1);
             System.out.println("Máx dítigos fracionários: "+numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(valor));
         }
-
         System.out.println("----------------");
 
         String valorString = "1000.2130";
@@ -32,6 +28,5 @@ public class NumberFormatTest01 {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
     }
 }
