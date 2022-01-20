2389b9e806bec714f8545bab7b372f435fd33164
Invelox Tecnologias <contato@invelox.com.br>
2022-01-19 22:11:04 -0300

    116 - Classes Utilitárias - Internacionalização Números com Locale

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/LocaleTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest02 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        String[] isoCountries = Locale.getISOCountries();
+        String[] isoLanguages = Locale.getISOLanguages();
+
+        for (String isoCountry : isoCountries) {
+            System.out.println(isoCountry+" ");
+        }
+
+        System.out.println();
+
+        for (String isoLanguage : isoLanguages) {
+            System.out.println(isoLanguage+" ");
+        }
+    }
+}
