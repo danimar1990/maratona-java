0591a4169ef5acd62d42f356fd2cb4f989e0944c
Invelox Tecnologias <contato@invelox.com.br>
2022-01-20 23:56:24 -0300

    125 - Classes Utilitárias - ChronoUnit

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/ChronoUnitTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(1990, Month.FEBRUARY, 5, 0, 0);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println("Idade: "+ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
