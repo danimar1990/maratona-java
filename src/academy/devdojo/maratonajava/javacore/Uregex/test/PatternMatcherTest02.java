2d0738c223664020286fc31be6f9155d0a34d29d
Invelox Tecnologias <contato@invelox.com.br>
2022-01-26 22:13:28 -0300

    132 - Classes Utilitárias - Regex pt 02 - Pattern e Matcher - Meta caracteres

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -5,8 +5,15 @@ import java.util.regex.Pattern;
 
 public class PatternMatcherTest02 {
     public static void main(String[] args) {
-        String regex = "aba";
-        String texto = "abaaba";
+        // \d = Todos os dígitos
+        // \D = Tudo o que não for dígito
+        // \s = Todos os espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-ZA-Z, dígitos e _
+        // \W = Tudo o que não for incluso no \w
+
+        String regex = "\\W";
+        String texto = "@$#h_hj2 12g\tvh21";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("Texto....: "+texto);
