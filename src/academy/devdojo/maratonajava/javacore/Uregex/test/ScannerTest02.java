db2740f494e202b091d4ce219bc93d9f27ef450f
Invelox Tecnologias <contato@invelox.com.br>
2022-01-31 23:27:18 -0300

    137 - Classes Utilitárias - Scanner - Tokens e Delimitadores

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest02.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.Scanner;
+
+public class ScannerTest02 {
+    public static void main(String[] args) {
+        String texto = "Caranthir, Eredin, Imlerith, Avallac'c, true, 200";
+        Scanner scanner = new Scanner(texto);
+        scanner.useDelimiter(",");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+                int i = scanner.nextInt();
+                System.out.println("Int: "+i);
+            } else if(scanner.hasNextBoolean()) {
+                 boolean b = scanner.nextBoolean();
+                 System.out.println("Boolean: "+b);
+            } else {
+                System.out.println(scanner.next());
+            }
+        }
+
+
+
+    }
+}
